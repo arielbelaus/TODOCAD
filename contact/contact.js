27d4ca9f87682f62551d6aa7e867b3f6077b6fd7
arielbelaus <68301852+arielbelaus@users.noreply.github.com>
2020-09-21 09:08:35 -0300

    Quinto commit

---
+++ contact/contact.js
@@ -0,0 +1,11 @@
+function sendMail(){
+    const name = document.querySelector('#name').value
+    const email = document.querySelector('#email').value
+    const subject = document.querySelector('#subject').value
+    const body = document.querySelector('#textarea').value
+    window.open(`mailto:example@gmail.com?subject=${subject}&body=${body}. Sent by: ${name}. Email to contact me: ${email}`);
+    name.value = ''
+    email.value = ''
+    subject.value = ''
+    body.value = ''
+}
