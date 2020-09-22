7d4fa6839d9fec57f8cd7e12bf99b9f063b3c57a
arielbelaus <68301852+arielbelaus@users.noreply.github.com>
2020-09-21 22:01:55 -0300

    Sexto Commit

--- contact/contact.js
+++ contact/contact.js
@@ -3,7 +3,7 @@ function sendMail(){
     const email = document.querySelector('#email').value
     const subject = document.querySelector('#subject').value
     const body = document.querySelector('#textarea').value
-    window.open(`mailto:example@gmail.com?subject=${subject}&body=${body}. Sent by: ${name}. Email to contact me: ${email}`);
+    window.open(`mailto:info@todocad.com?subject=${subject}&body=${body}. Sent by: ${name}. Email to contact me: ${email}`);
     name.value = ''
     email.value = ''
     subject.value = ''
